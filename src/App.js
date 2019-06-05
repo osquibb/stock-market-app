e384b97194034c632080ec89431af6067211e74e
Oliver Squibb <osquibb@gmail.com>
2019-06-05 16:15:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'MA4OHKLGGWPC2AJD';
+
+function logStockData(symbol) {
+  fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+}
+
+
 function App() {
+
+  // API TESTING...
+
+  logStockData('AAPL');
+
+  // ...API TESTING
+
   return (
     <div className="App">
       <header className="App-header">
