78cdda94542cb87572060a5c517719df8f0b5742
Oliver Squibb <osquibb@gmail.com>
2019-06-14 11:58:11 -0400

    Updates

---
+++ precache-manifest.271a738bb533e834f07cb622572266ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3b4b6e54fa1a0939378dbdd1dd9ab3",
+    "url": "/trading-app/index.html"
+  },
+  {
+    "revision": "2c090f670856806377f8",
+    "url": "/trading-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cd9011388276e33216ee",
+    "url": "/trading-app/static/css/main.ad9543ac.chunk.css"
+  },
+  {
+    "revision": "2c090f670856806377f8",
+    "url": "/trading-app/static/js/2.8b2dbff1.chunk.js"
+  },
+  {
+    "revision": "cd9011388276e33216ee",
+    "url": "/trading-app/static/js/main.d2b16453.chunk.js"
+  },
+  {
+    "revision": "bfb5056b6bd73c5c34c5",
+    "url": "/trading-app/static/js/runtime~main.ef3f9a93.js"
+  }
+]);
